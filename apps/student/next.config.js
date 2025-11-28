6f32ecad5b119786eaa79432d1756021564587cd
Marlionmac <marlionmac@Marlionmacs-Mac-mini.local>
2025-11-28 14:38:04 +0530

    Configure for Digital Ocean deployment with standalone output

--- apps/student/next.config.js
+++ apps/student/next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   transpilePackages: ['@marlion/ui', '@marlion/lib', '@marlion/config'],
   experimental: {
     serverActions: {
